fef6dbbb3fb9b92bf3534a870c434f6143f5646c
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2021-07-18 11:52:17 -0300

    refactor: moved more code to setup; fix buba black line

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-let model, maxPredictions;
+let model;
 
 const modelURL = "./model/model.json";
 const metadataURL = "./model/metadata.json";
 
-const loadFile = (dropEvent) => {
-  startLoading();
-  let files = dropEvent.dataTransfer.files;
-  for (let i = 0, file; (file = files[i]); i++) {
-    if (file.type.match(/image.*/)) {
-      let reader = new FileReader();
-
-      reader.onload = async function (droppedImage) {
-        predictionImage.src = droppedImage.target.result;
-
-        setTimeout(async () => {
-          await predict();
-        }, 1500);
-      };
-
-      reader.readAsDataURL(file);
-    }
-  }
-};
-
 const loadModel = async () => {
   model = await tmImage.load(modelURL, metadataURL);
-  maxPredictions = model.getTotalClasses();
 };
 
 loadModel();
 
-async function predict() {
+async function classify() {
   const predictions = await model.predict(predictionImage);
   const res = predictions.reduce((a, b) =>
     a.probability > b.probability ? a : b
@@ -38,3 +17,7 @@ async function predict() {
 
   displayResults(res);
 }
+
+const onImageLoaded = async () => {
+  await classify();
+};
--- scripts/setup2.js
+++ scripts/setup2.js
@@ -2,7 +2,23 @@ const onDrop = (dropEvent) => {
   dropEvent.stopPropagation();
   dropEvent.preventDefault();
 
-  loadFile(dropEvent);
+  startLoading();
+
+  let file = dropEvent.dataTransfer.files[0];
+
+  if (file.type.match(/image.*/)) {
+    let reader = new FileReader();
+
+    reader.onload = async function (droppedImage) {
+      predictionImage.src = droppedImage.target.result;
+
+      setTimeout(async () => {
+        await onImageLoaded();
+      }, 1500);
+    };
+
+    reader.readAsDataURL(file);
+  }
 };
 
 dropZone.addEventListener("drop", onDrop);
