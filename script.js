32d631f45190609c6dee73296a83868cbd608f6c
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2021-07-18 10:40:30 -0300

    refactor: updateLoading func; code cleanup

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-let timer = null;
-let counter = 1;
-
 const img = document.getElementById("img-pokemon");
 const predictionImage = document.getElementById("predict-img");
 const result = document.getElementById("result");
 let dropZone = document.getElementById("app");
 
-const startLoading = () => {
-  img.classList.remove("hidden");
-  result.classList.add("hidden");
-  timer = setInterval(() => {
+let counter = 1;
+let model, webcam, labelContainer, maxPredictions;
+const modelURL = "./model/model.json";
+const metadataURL = "./model/metadata.json";
+let isLoading = false;
+
+function updateLoading() {
+  if (isLoading) {
     img.src = `./assets/silhueta${counter}.png`;
     counter = counter === 4 ? 1 : counter + 1;
-  }, 150);
+    setTimeout(() => {
+      window.requestAnimationFrame(updateLoading);
+    }, 100);
+  }
+}
+
+const startLoading = () => {
+  isLoading = true;
+  updateLoading();
+  img.classList.remove("hidden");
+  result.classList.add("hidden");
 };
 
 const stopLoading = () => {
   result.classList.remove("hidden");
   img.classList.remove("hidden");
-  clearInterval(timer);
+  isLoading = false;
 };
 
-const showResults = ({ className, probability }) => {
+const displayResults = ({ className, probability }) => {
   stopLoading();
   img.src = `./assets/${className}.png`;
-  result.innerText = `Tenho ${(probability * 100).toFixed(2)}% de certeza de que é um ${className}`
+  result.innerText = `Tenho ${(probability * 100).toFixed(
+    2
+  )}% de certeza de que é um ${className}`;
 };
-
-// startLoading();
-// setTimeout(() => {
-//   result.classList.remove("hidden");
-//   img.src = `./assets/pikachu.png`;
-//   stopLoading();
-// }, 5000);
 
 dropZone.addEventListener("dragover", function (e) {
   e.stopPropagation();
@@ -40,36 +46,27 @@ dropZone.addEventListener("dragover", function (e) {
   e.dataTransfer.dropEffect = "copy";
 });
 
-dropZone.addEventListener("drop", function (e) {
-  e.stopPropagation();
-  e.preventDefault();
+dropZone.addEventListener("drop", function (dropEvent) {
+  dropEvent.stopPropagation();
+  dropEvent.preventDefault();
   startLoading();
-  let files = e.dataTransfer.files;
+  let files = dropEvent.dataTransfer.files;
   for (let i = 0, file; (file = files[i]); i++) {
     if (file.type.match(/image.*/)) {
       let reader = new FileReader();
 
-      reader.onload = async function (e2) {
-        // let img = document.createElement("img");
-        predictionImage.src = e2.target.result;
-        // document.body.appendChild(img);
+      reader.onload = async function (droppedImage) {
+        predictionImage.src = droppedImage.target.result;
+
         setTimeout(async () => {
-            await predict();
-            stopLoading();
-        }, 2000);
+          await predict();
+        }, 1500);
       };
 
       reader.readAsDataURL(file);
     }
   }
 });
-
-const URL = "./model/";
-
-let model, webcam, labelContainer, maxPredictions;
-
-const modelURL = URL + "model.json";
-const metadataURL = URL + "metadata.json";
 
 (async () => {
   model = await tmImage.load(modelURL, metadataURL);
@@ -82,5 +79,5 @@ async function predict() {
     a.probability > b.probability ? a : b
   );
 
-  showResults(res);
+  displayResults(res);
 }
